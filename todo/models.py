fbc8658d17c7de847a98db1cfdcc93f13ec2f4f1
dickson7 <dickson@whaleandjaguar.co>
2022-08-18 07:26:42 -0500

    feat: initial migrations

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from authentication.models import User
+from helpers.models import TrackingModels
+
+class Todo(TrackingModels):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    is_completed = models.BooleanField(default=False)
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
