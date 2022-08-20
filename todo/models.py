a687a7b50785bbc21798c60fae5daab649c0d9ef
dickson7 <dickson@whaleandjaguar.co>
2022-08-20 07:20:54 -0500

    feat: Login and registration with user association and view protection

--- todo/models.py
+++ todo/models.py
@@ -7,7 +7,7 @@ class Todo(TrackingModels):
     title = models.CharField(max_length=255)
     description = models.TextField()
     is_completed = models.BooleanField(default=False)
-    #owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.title
