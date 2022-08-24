7e970ca498fa1dd9f14e0c9389fcf3f8fce2d606
dickson7 <dickson@whaleandjaguar.co>
2022-08-24 12:22:37 -0500

    feat: workflows

--- todosite/settings.py
+++ todosite/settings.py
@@ -140,4 +140,6 @@ EMAIL_PORT = 587
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR, 'todosite/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'todosite/static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
